5ba35c1245aa90104d2a14c54bdecdf3d2725e20
zhyyu <zhyyu2016@gmail.com>
2019-06-21 23:49:56 +0800

    1.增加groovy 依赖;2.@SpringbootApplication;3.test application.properties

--- src/main/java/com/zhyyu/learn/learnspringboot/LearnSpringbootApplication.java
+++ src/main/java/com/zhyyu/learn/learnspringboot/LearnSpringbootApplication.java
@@ -3,6 +3,14 @@ package com.zhyyu.learn.learnspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * LearnSpringbootApplication
+ * <pre>
+ *      @SpringBootApplication javadoc:
+ *      This is a convenience annotation that is equivalent to declaring @Configuration, @EnableAutoConfiguration and @ComponentScan.
+ * </pre>
+ */
 @SpringBootApplication
 public class LearnSpringbootApplication {
 
