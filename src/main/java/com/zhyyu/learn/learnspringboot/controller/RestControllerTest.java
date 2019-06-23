8160a749d75a48806d9ff90be2a8f9ee0fd3ef9a
zhyyu <zhyyu2016@gmail.com>
2019-06-23 16:14:26 +0800

    RestControllerTest

---
+++ src/main/java/com/zhyyu/learn/learnspringboot/controller/RestControllerTest.java
@@ -0,0 +1,22 @@
+package com.zhyyu.learn.learnspringboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * RestControllerTest
+ * <pre>
+ *     1. 访问 http://localhost:8000/test/hello, 返回 hello 字符串
+ *     2. tarter-web 自动帮配置好DispatcherServlet 等
+ * </pre>
+ */
+@RestController
+@RequestMapping("/test")
+public class RestControllerTest {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello!";
+    }
+
+}
