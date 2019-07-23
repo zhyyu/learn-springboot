312cae7ae05d00562348f1421725622db4a7bbde
zhyyu <zhyyu2016@gmail.com>
2019-07-23 13:35:19 +0800

    wireshark multipartfile

--- src/main/java/com/zhyyu/learn/learnspringboot/controller/RestControllerTest.java
+++ src/main/java/com/zhyyu/learn/learnspringboot/controller/RestControllerTest.java
@@ -2,6 +2,7 @@ package com.zhyyu.learn.learnspringboot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -21,12 +22,17 @@ public class RestControllerTest {
 
     @RequestMapping("/hello")
     public String hello(HttpServletResponse response) {
-        Cookie cookie = new Cookie("cookie-key", "cookie-value");
+//        Cookie cookie = new Cookie("cookie-key", "cookie-value");
 //        cookie.setPath("xpath");
 //        cookie.setDomain("google.com");
 
-        response.addCookie(cookie);
+//        response.addCookie(cookie);
         return "hello!";
+    }
+
+    @RequestMapping("/multipartFile")
+    public String multipartFile(MultipartFile file, MultipartFile pic, String field1) {
+        return "name: " + file.getName() + ", filename: " + file.getOriginalFilename() + ", field1: " + field1 + ", pic filename:" + pic.getOriginalFilename();
     }
 
 }
