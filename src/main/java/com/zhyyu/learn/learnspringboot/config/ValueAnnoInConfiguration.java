bb81e2a9f121eb33505f35c278df4d7df4b920d4
juror.yu <juror.yu@mart.blue>
2019-08-13 16:24:58 +0800

    ValueAnnoInConfiguration

---
+++ src/main/java/com/zhyyu/learn/learnspringboot/config/ValueAnnoInConfiguration.java
@@ -0,0 +1,29 @@
+package com.zhyyu.learn.learnspringboot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author juror
+ * @datatime 2019/8/13 14:40
+ */
+@Configuration
+public class ValueAnnoInConfiguration {
+
+    @Bean
+    public MyObj getMyObj(@Value("${server.port}") Integer port) {
+        System.out.println("myobj port: " + port);
+        return new MyObj();
+    }
+
+    public static class MyObj {
+        @PostConstruct
+        public void init() {
+            System.out.println("MyObj inited");
+        }
+    }
+
+}
